0fd0e4b7990043c046bc84fec2f04c7ef40af817
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2020-07-02 13:46:56 +0200

    f-Homepage

---
+++ front/src/components/Home/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import AdventureSmall from 'src/components/Home/AdventureSmall';
+
+import './home.scss';
+
+const Home = () => (
+  <main className="home">
+    <section className="presentation">
+      <h1 className="presentation-title">
+        Un titre cool
+      </h1>
+      <p className="presentation-description">
+        Un texte de présentation qui claque !! Lorem ipsum dolor sit, amet consectetur adipisicing
+        Quam unde aspernatur distinctio quae voluptatem id ipsa adipisci, provident ipsum officia
+        eius itaque quibusdam architecto nam facilis impedit, perferendis laborum ullam?
+      </p>
+      <img
+        className="presentation-image"
+        src=""
+        alt="Présentation du site"
+      />
+    </section>
+    <section className="latest-adventures">
+      <h2>
+        Découvrez les dernières aventures crées par les joueurs !
+      </h2>
+      <AdventureSmall />
+      <AdventureSmall />
+      <AdventureSmall />
+    </section>
+  </main>
+);
+
+export default Home;
