f72bf7eb1e7b0ba3fd814954b64fafa3d6e54b9c
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2020-07-02 11:14:23 +0200

    f- react architecture

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -2,14 +2,12 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
-// == Composant
+// == Components
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <h1>O'ld the door</h1>
   </div>
 );
 
