fbfa3a2663a06c5cd5b3289cf0c2740d5d424238
Jukka Pasanen <pasanen.jukka11@gmail.com>
2020-07-02 13:50:22 +0200

    sorry, i'll think of using pull requests next time

--- front/src/components/ChapterCreate/index.js
+++ front/src/components/ChapterCreate/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 const ChapterCreate = () => (
 
   <main>
-    <form action="submit">
-      <input type="text" id="choixEdition" name="choixEdition" />
-      <input type="text" id="chapitreParent" name="chapitreParent" />
-      <input type="text" id="nomChapitre" name="nomChapitre" />
-      <input type="text" id="texteChapitre" name="texteChapitre" />
+    <form action="submit" name="chapterCreate">
+      <input type="text" id="chapterCreate-edit" name="chapterCreate-edit" />
+      <input type="text" id="chapterCreate-parent" name="chapterCreate-parent" />
+      <input type="text" id="chapterCreate-name" name="chapterCreate-name" />
+      <input type="text" id="chapterCreate-text" name="chapterCreate-text" />
 
-      <input type="text" id="clef" name="clef" />
-      <input type="text" id="serrure" name="serrure" />
-      <div className="messageReussite"> message de reussite</div>
+      <input type="text" id="chapterCreate-key" name="chapterCreate-key" />
+      <input type="text" id="chapterCreate-lock" name="chapterCreate-lock" />
+      <div className="winningMessage"> message de reussite</div>
 
       <button type="submit"> Chapitre suivant</button>
       <button type="submit"> Fin de l'aventure</button>
--- front/src/components/StoryCreate/index.js
+++ front/src/components/StoryCreate/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 const StoryCreate = () => (
 
   <main>
-    <form action="submit">
-      <input type="text" id="title" name="title" />
-      <input type="text" id="synopsis" name="synopsis" />
-      <input type="text" id="description" name="description" />
+    <form action="submit" name="storyCreate">
+      <input type="text" id="storyCreate-title" name="storyCreate-title" />
+      <input type="text" id="storyCreate-synopsis" name="storyCreate-synopsis" />
+      <input type="text" id="storyCreate-description" name="storyCreate-description" />
       <button type="submit">C'est parti</button>
     </form>
   </main>
