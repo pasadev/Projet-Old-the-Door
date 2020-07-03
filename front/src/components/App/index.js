804f429742ef1887e4a3b8d078be6a65d45596ff
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2020-07-03 10:21:13 +0200

    f-font

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -4,12 +4,11 @@ import React from 'react';
 // == Import : local
 // Components
 import Home from 'src/components/Home';
-import Connexion from'src/components/Connexion';
+// import Connexion from'src/components/Connexion';
 
 // import Register from 'src/components/Register';
 // import Adventures from 'src/components/Adventures';
 import Team from 'src/components/Team';
-
 
 // == Import
 import './styles.scss';
