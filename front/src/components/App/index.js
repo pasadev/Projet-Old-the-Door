e0afac6b8721b808edec57b91d1f88e3437421bc
Jukka Pasanen <pasanen.jukka11@gmail.com>
2020-07-03 11:56:43 +0200

    initial css for gamescreen done, inspired by old black and green terminals

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 // == Import : local
 // Components
-import Home from 'src/components/Home';
-import Connexion from'src/components/Connexion';
+// import Home from 'src/components/Home';
+// import Connexion from'src/components/Connexion';
+import Game from 'src/components/Game';
 
 // import Register from 'src/components/Register';
 // import Adventures from 'src/components/Adventures';
-import Team from 'src/components/Team';
-
+// import Team from 'src/components/Team';
 
 // == Import
 import './styles.scss';
@@ -17,8 +17,7 @@ import './styles.scss';
 // == Components
 const App = () => (
   <div className="app">
-    <Home />
-    <Team />
+    <Game />
   </div>
 );
 
--- front/src/components/Game/index.js
+++ front/src/components/Game/index.js
@@ -1,24 +1,48 @@
+/* eslint-disable max-len */
 import React from 'react';
+
+import './game.scss';
 
 const GameScreen = () => (
 
   <main>
-    <div>
-      <form>
-        <label htmlFor="game-key"> Déclencheur</label>
-        <input name="game-key" id="game-key" placeholder="key" />
+    <div className="pccase">
+      <div className="screen oldscreeneffect">
+        <div className="screentext">
+          <div className="align-content">
+            <div className="align-left"><p>Initializing 'O'ld the door' v0.01 ...........................................................</p>
+              <p>
+                █░█░░░█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ ░█<br />
+                █░█▄█▄█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ ░█<br />
+                █░░▀░▀░ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ ░█<br />
+                <br />Damien Toscano &lt;certified cyborg&gt;<br />gone back in time<br />___________________________________________________________
+              </p>
+            </div>
+            <div className="game-timer align-right">15:05</div>
+          </div>
 
-        <label htmlFor="game-lock"> Support</label>
-        <input name="game-lock" id="game-lock" placeholder="lock" />
-      </form>
+          <form>
+            <div>
+              <label htmlFor="game-key"> Déclencheur</label> <br />
+              <input name="game-key" id="game-key" placeholder="key" className="screentext" />
+            </div>
+            <div>
+              <label htmlFor="game-lock"> Support</label> <br />
+              <input name="game-lock" id="game-lock" placeholder="lock" className="screentext" />
+            </div>
+          </form>
+          <p>
+            N’empêche que tout le monde parle de moi! C’est quand même un signe! Allez-y mollo avec la joie! Ah ah Sire! Je vous attends! À moins que vous préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre!<br />
+
+            Léodagan et moi on fait semblant de vous prendre en otage. Ah ah Sire! Je vous attends! À moins que vous préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre! Perceval le Galois en tout cas tout le monde s’accorde à dire que c’est une tanche et ça c’est pas une légende! Oh ben ça va on plaisante! Sire, mon père était peut-être unijambiste mais, moi, ma femme a pas de moustache!<br />
+
+            Non mais maintenant il faut se tirer dans l'autre sens. Ben je suis pas mystérieux moi! J’suis même pas solitaire.<br />
+          </p>
+
+        </div>
+      </div>
+
     </div>
-    <button type="submit">Tester</button>
-    <h1>nom de chapitre</h1>
-
-    <div className="game-adventureText">This is where text goes.</div>
-
-    <Timer />
-    <div>ici un bouton pour remonter en haut de la page</div>
   </main>
 
 );
