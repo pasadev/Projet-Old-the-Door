e2e56b2f05168d80ccda88c606a915b37cce85e6
HugoLiger <hugo.le.liger@gmail.com>
2020-07-03 10:57:54 +0200

    F-Footer

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -9,16 +9,18 @@ import Connexion from'src/components/Connexion';
 // import Register from 'src/components/Register';
 // import Adventures from 'src/components/Adventures';
 import Team from 'src/components/Team';
-
+// import Footer from 'src/components/Footer';
 
 // == Import
 import './styles.scss';
 
 // == Components
 const App = () => (
-  <div className="app">
-    <Home />
-    <Team />
+  <div className="Gaming">
+    <div className="app">
+      <Home />
+      <Team />
+    </div>
   </div>
 );
 
---
+++ front/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <h1 className="title">O'ld The Door</h1>
+    <p>notre équipe</p>
+    <p>&copy; 2020 lateamdefou.com</p>
+  </footer>
+);
+export default Footer;
