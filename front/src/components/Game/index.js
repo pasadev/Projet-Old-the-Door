bd03e3fb53eb84d75629f90bf851d1b418c0fe51
Jukka Pasanen <pasanen.jukka11@gmail.com>
2020-07-03 15:03:55 +0200

    responsive css for gamescreen

--- front/src/components/Game/index.js
+++ front/src/components/Game/index.js
@@ -11,7 +11,7 @@ const GameScreen = () => (
         <div className="screentext">
           <div className="align-content">
             <div className="align-left"><p>Initializing 'O'ld the door' v0.01 ...........................................................</p>
-              <p>
+              <p className="optional-welcome">
                 █░█░░░█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ ░█<br />
                 █░█▄█▄█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ ░█<br />
                 █░░▀░▀░ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ ░█<br />
@@ -31,7 +31,7 @@ const GameScreen = () => (
               <input name="game-lock" id="game-lock" placeholder="lock" className="screentext" />
             </div>
           </form>
-          <p>
+          <p className="game-text">
             N’empêche que tout le monde parle de moi! C’est quand même un signe! Allez-y mollo avec la joie! Ah ah Sire! Je vous attends! À moins que vous préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre!<br />
 
             Léodagan et moi on fait semblant de vous prendre en otage. Ah ah Sire! Je vous attends! À moins que vous préfériez qu’on dise partout que le roi est une petite pédale qui pisse dans son froc à l’idée d’se battre! Perceval le Galois en tout cas tout le monde s’accorde à dire que c’est une tanche et ça c’est pas une légende! Oh ben ça va on plaisante! Sire, mon père était peut-être unijambiste mais, moi, ma femme a pas de moustache!<br />
