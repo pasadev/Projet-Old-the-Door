556c01dc1f7130097313ef6051784c318fb36cba
Jukka Pasanen <pasanen.jukka11@gmail.com>
2020-07-06 12:09:54 +0200

    premiere etape

--- front/src/components/Connexion/index.js
+++ front/src/components/Connexion/index.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Connexion = () => (
-  <main>
-    <h1>Connexion</h1>
-    <form action="submit" name="connexion">
-      <input type="text" id="connexion-email" name="connexion-email" />
-      <input type="text" id="connexion-password" name="connexion-password" />
-      <button type="submit">Sign In</button>
-    </form>
-  </main>
-);
+// import { useField } from './hooks';
+
+import './connexion.scss';
+
+const Connexion = ({
+  email,
+  password,
+  // changeField,
+  handleLogin,
+  handleLogout,
+  isLogged,
+  loggedMessage,
+}) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleLogin();
+  };
+
+  return (
+    <div className="login-form">
+      {isLogged && (
+        <div className="login-form-logged">
+          <p className="login-form-message">
+            {loggedMessage}
+          </p>
+          <button
+            type="button"
+            className="login-form-button"
+            onClick={handleLogout}
+          >
+            Déconnexion
+          </button>
+        </div>
+      )}
+      {!isLogged && (
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <input type="text" id="connexion-email" name="connexion-email" value={email} />
+          <input type="text" id="connexion-password" name="connexion-password" value={password} />
+          <button type="submit">Sign In</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+Connexion.propTypes = {
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  // changeField: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  handleLogout: PropTypes.func.isRequired,
+  isLogged: PropTypes.bool,
+  loggedMessage: PropTypes.string,
+};
+
+Connexion.defaultProps = {
+  isLogged: false,
+  loggedMessage: 'Connecté',
+};
+
 export default Connexion;
--- front/src/reducers/user.js
+++ front/src/reducers/user.js
@@ -1,19 +1,31 @@
-import { DO_SOMETHING } from 'src/actions/user';
+import { UPDATE_USER_FIELD, SAVE_USER } from 'src/actions/user';
 
 const initialState = {
-
+  // contenu de l'input pour l'adresse e-mail
+  email: '',
+  // contenu de l'input pour le mot de passe
+  password: '',
+  // informations sur l'utilisateur
+  info: {},
+  // indique si l'utilisateur est loggué
+  isLogged: false,
 };
 
 const user = (state = initialState, action = {}) => {
   switch (action.type) {
-    case DO_SOMETHING:
-      // on retourne un nouveau state
+    case UPDATE_USER_FIELD:
       return {
-        // en déversant les informations du state actuel
         ...state,
-        // et en appliquant des modifications
-        propriété_à_modifier_1: 'valeur',
-        propriété_à_modifier_2: action.newValue,
+        [action.name]: action.newValue,
+      };
+
+    case SAVE_USER:
+      return {
+        ...state,
+        info: action.data,
+        isLogged: action.isLogged,
+        email: '',
+        password: '',
       };
 
     default: return state;
@@ -21,4 +33,3 @@ const user = (state = initialState, action = {}) => {
 };
 
 export default user;
-
