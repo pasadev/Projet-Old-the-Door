3db9c2a858ffd40d9b228294b61489151eb641f3
DamienToscano <toscano.damien@gmail.com>
2020-07-07 16:03:40 +0200

    b-Adding properties to Story form

--- back/src/Form/StoryType.php
+++ back/src/Form/StoryType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Chapter;
 use App\Entity\Story;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +18,35 @@ class StoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('synopsis')
-            ->add('active')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('firstChapter')
-            ->add('author')
+            ->add(
+                'title',
+                TextType::class,
+                )
+            ->add(
+                'synopsis',
+                TextareaType::class,
+                )
+            ->add(
+                'active',
+                IntegerType::class,
+                )
+            ->add(
+                'firstChapter',
+                EntityType::class,
+                [
+                    'class' => Chapter::class,
+                    //On utilise une requête personnalisé, voir si on en a besoin
+                    /* 'choice_label' => 'title' */
+                ]
+                )
+            ->add(
+                'author',
+                EntityType::class,
+                [
+                    //On utilise une requête personnalisé, voir si on en a besoin
+                    /* 'choice_label' => 'username' */
+                ]
+                )
         ;
     }
 
