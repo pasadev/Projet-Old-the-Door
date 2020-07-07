c141c304b3b8c1a7d59b6cac061f9716187c76a3
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2020-07-07 09:25:28 +0200

    f-base css homepage

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -1,37 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import AdventureSmall from 'src/components/AdventureSmall';
+import AdventureSmall from 'src/containers/AdventureSmall';
 
 import './home.scss';
 
-const Home = () => (
+const Home = ({ /*adventures*/ }) => (
   <main className="home">
     <section className="presentation">
       <h1 className="presentation-title">
-        Un titre cool
+        Bienvenue sur le site d'aventures O'ld the door
       </h1>
+      <span className="spacer">--------------------------------------</span>
+      <p className="asciiart" />
       <p className="presentation-description">
         Un texte de présentation qui claque !! Lorem ipsum dolor sit, amet consectetur adipisicing
         Quam unde aspernatur distinctio quae voluptatem id ipsa adipisci, provident ipsum officia
         eius itaque quibusdam architecto nam facilis impedit, perferendis laborum ullam?
       </p>
-      <img
-        className="presentation-image"
-        src=""
-        alt="Présentation du site"
-      />
+      <span className="spacer">--------------------------------------</span>
     </section>
     <section className="latest-adventures">
-      <h2>
+      <h2 className="latest-adventures-title">
         Découvrez les dernières aventures crées par les joueurs !
       </h2>
+      <span className="spacer">--------------------------------------</span>
       <div className="adventureSmall-container">
-        <AdventureSmall />
-        <AdventureSmall />
-        <AdventureSmall />
+        {/* {adventures.map((adventure) => (
+          <AdventureSmall {...adventure} key={adventure.id} />
+        ))} */}
       </div>
     </section>
   </main>
 );
+
+// Home.propTypes = {
+//   adventures: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//     }).isRequired,
+//   ).isRequired,
+// };
 
 export default Home;
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user';
 import burgerMenuReducer from './burgerMenu';
+import adventuresReducer from './adventures';
 
 const rootReducer = combineReducers({
   user: userReducer,
   burgerMenu: burgerMenuReducer,
+  adventures: adventuresReducer,
 });
 
 export default rootReducer;
