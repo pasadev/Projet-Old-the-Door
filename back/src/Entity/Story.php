5198a207d7d2ebf3ffce2555fee9dd58f007ae8a
DamienToscano <toscano.damien@gmail.com>
2020-07-08 14:24:14 +0200

    b-Add slug property for story

--- back/src/Entity/Story.php
+++ back/src/Entity/Story.php
@@ -91,6 +91,11 @@ class Story
      */
     private $hasParties;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->hasChapters = new ArrayCollection();
@@ -244,6 +249,18 @@ class Story
                 $hasParty->setForStory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
