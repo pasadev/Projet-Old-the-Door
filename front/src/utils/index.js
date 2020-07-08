ffff344891ae782e2d767a351f8f173a6c639b54
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2020-07-08 19:24:03 +0200

    f-adventure page with small css and slug working

--- front/src/utils/index.js
+++ front/src/utils/index.js
@@ -1,9 +1,10 @@
 import slugify from 'slugify';
 
+// eslint-disable-next-line import/prefer-default-export
 export const slugifyTitle = (title) => (
   slugify(title, { lower: true })
 );
 
-export const getAdventureBySlug = (adventures, slug) => (
-  adventures.find((adventure) => slugifyTitle(adventure.title) === slug)
-);
+// export const getAdventureBySlug = (adventures, slug) => (
+//   adventures.find((adventure) => slugifyTitle(adventure.title) === slug)
+// );
