5f00553fa06e1110df65e6e40dd412280c56af4b
Max-ans <maxenceroyer@hotmail.com>
2020-07-16 14:40:19 +0200

    delete redux react session

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { sessionReducer } from 'redux-react-session';
 import userReducer from './user';
 import utilsReducer from './utils';
 import adventuresReducer from './adventures';
@@ -7,25 +6,13 @@ import gameScreenReducer from './gameScreen';
 import storyCreationReducer from './storyCreation';
 import storyEditReducer from './storyEdit';
 
-const reducers = {
+const rootReducer = combineReducers({
   user: userReducer,
   utils: utilsReducer,
   adventures: adventuresReducer,
   gameScreen: gameScreenReducer,
   storyCreation: storyCreationReducer,
   storyEdit: storyEditReducer,
-  session: sessionReducer,
-};
-const reducer = combineReducers(reducers);
+});
 
-// const rootReducer = combineReducers({
-//   session: sessionReducer,
-//   user: userReducer,
-//   utils: utilsReducer,
-//   adventures: adventuresReducer,
-//   gameScreen: gameScreenReducer,
-//   storyCreation: storyCreationReducer,
-//   storyEdit: storyEditReducer,
-// });
-
-export default reducer;
+export default rootReducer;
