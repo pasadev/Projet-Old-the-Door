007a6f82ced26bab29cf150e1ea9520bfb0e17b9
Max-ans <maxenceroyer@hotmail.com>
2020-07-22 20:42:53 +0200

    control access on showBySlug method

--- back/src/Security/Voter/StoryVoter.php
+++ back/src/Security/Voter/StoryVoter.php
@@ -12,7 +12,7 @@ class StoryVoter extends Voter
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, ['edit', 'add', 'delete', 'active'])
+        return in_array($attribute, ['edit', 'add', 'delete', 'active', 'showBySlug'])
             && $subject instanceof \App\Entity\Story;
     }
 
@@ -21,14 +21,15 @@ class StoryVoter extends Voter
         
         $user = $token->getUser();
      
-        // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
-            return false;
-        }
+       
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
             case 'edit':
+                // if the user is anonymous, do not grant access
+                if (!$user instanceof UserInterface) {
+                    return false;
+                }
                 // A user can edit a story IF he is logged and the story's author
                 if($user->getIsLogged() && $user->getId() === $subject->getAuthor()->getId()) {
                     // return true or false
@@ -37,6 +38,10 @@ class StoryVoter extends Voter
                 break;
             
             case 'add':
+                // if the user is anonymous, do not grant access
+                if (!$user instanceof UserInterface) {
+                    return false;
+                }
                 // if user is logged
                 if($user->getIsLogged()) {
                     return true;
@@ -44,6 +49,10 @@ class StoryVoter extends Voter
                 break;
 
             case 'delete':
+                // if the user is anonymous, do not grant access
+                if (!$user instanceof UserInterface) {
+                    return false;
+                }
                 // if user is logged
                 if ($user->getIsLogged() && $user->getId() === $subject->getAuthor()->getId()) {
                     return true;
@@ -51,11 +60,31 @@ class StoryVoter extends Voter
                 break;
                 
             case 'active':
+                // if the user is anonymous, do not grant access
+                if (!$user instanceof UserInterface) {
+                    return false;
+                }
                 // Eventually allow admin users to active / desactive a story later
                 // if user is logged and author
                 if ($user->getIsLogged() && $user->getId() === $subject->getAuthor()->getId()) {
                     return true;
                 }
+                break;
+
+            case 'showBySlug':
+              
+                // if the story isn't active 
+                if (!$subject->getActive()){
+                    // and if story author isn't the user who try access to story
+                    if ($subject->getAuthor()->getId() !== $user->getId()){
+                        
+                        return false;
+                    }    
+                }
+  
+                // if story is active , everybody can see details of story
+                  return true;  
+         
                 break;
         }
 
